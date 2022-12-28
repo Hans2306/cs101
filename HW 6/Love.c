ed8ba971ffbb5363cba64efc2e6bddc4bc781fdd
Hans2306 <117811104+Hans2306@users.noreply.github.com>
2022-12-28 10:41:48 +0800

    Create Love.c

---
+++ HW 6/Love.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    
+    if (a == 3) {
+        printf ("Love\n");
+    } else if (a == 5) {
+        printf ("IU\n");
+    } else if (a == 15) {
+        printf ("Love IU\n");
+    } else {
+        printf ("%d\n",a);
+    }
+    
+    return 0;
+}
